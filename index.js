0e3d5603d9d0c5569ef5b2a6ecc6df872920cf01
Kàrèèm_Wèst💔-XMD🔥 <karimomary1511@gmail.com>
2025-07-07 19:27:38 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+console.log("✅ Bot started successfully! Welcome to Kareem_XMD_TECH 🚀");
+
+// TODO: Initialize your bot logic here
+// Example:
+// const { startBot } = require('./bot');
+// startBot();
+
+// You can add more handlers or features below
+// function handleIncomingMessage(message) {
+//   console.log(`📩 New incoming message: ${message}`);
+// }
